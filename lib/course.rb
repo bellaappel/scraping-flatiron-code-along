81df8e456d84b4dc3b7ae404e7e854c2390ea3cb
bellaappel <bellaappel0@gmail.com>
2020-10-19 12:41:32 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -5,13 +5,11 @@ class Course
     def initialize
         @@all << self
     end
-    
+
     def self.all
         @@all
     end
-
     def self.reset_all
         @@all.clear
     end
 end
-
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,17 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
+      # more code coming soon!
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      # doc.css(".post").each do |post|
+      #   course = Course.new
+      #   course.title = post.css("h2").text
+      #   course.schedule = post.css(".date").text
+      #   course.description = post.css("p").text
+       
   end
+
   def get_courses
     self.get_page.css(".post")
   end
@@ -37,4 +45,3 @@ class Scraper
 end
 
 Scraper.new.print_courses
-
